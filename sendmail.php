941f1bb53b6e3eb7b8fe11b9397398e9df5689ab
ilyacynep <105362934+ilyacynep@users.noreply.github.com>
2022-07-26 20:18:35 +0300

    Add files via upload

--- sendmail.php
+++ sendmail.php
@@ -1,13 +1,26 @@
 <?php
-    $tel = $_POST['input-tel'];
-    $email = $_POST['input-email'];
-    $name = $_POST['input-name'];
-    if($email===""){
-        $email="none";
-    }
-    $to = 'nobody@example.com';
-    $subject = 'the subject';
-    $message = "name: ".$name." tel: ".$tel." email: ".$email;
-    $headers = "this is header";
-    mail($to, $subject, $message, $headers);
-?>
+
+/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXX/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['input-name'];
+$phone = $_POST['input-tel'];
+$email = $_POST['input-email'];
+
+$token = "5599632446:AAHKT-sbxuNgPASMdV3UEC3frW4YRjwfwpE";
+$chat_id = "-625942042";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email: ' => $email,
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+if(isset($name)){
+    $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+}
+
+
+?> 
