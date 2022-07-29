05cb1ad4418ce05efd2aef6ad321c57852e01e6b
ilyacynep <105362934+ilyacynep@users.noreply.github.com>
2022-07-29 18:38:48 +0300

    fix 0.1
    fixed something bugs

--- src/js/app.js
+++ src/js/app.js
@@ -4,29 +4,30 @@ flsFunctions.isWebp();
 document.addEventListener("DOMContentLoaded", function () {
     var loading_progress = 0;
     var i = 1;
-
+    var error_tel = document.querySelector(".error-tel");
+    var error_email = document.querySelector(".error-email");
+    var error_name = document.querySelector(".error-name");
     var loading = document.querySelector(".loading");
-function myLoop() {         
-  setTimeout(function() { 
-    console.log('hello');
-    i++;                 
-      if (i < 100) {  
-        loading.innerHTML="0"+i
-      myLoop();             
-      } else {
-        loadingcomplete();   
-          loading.innerHTML = "100";
-      }          
-      
-  }, 0)
-}
+    function myLoop() {
+        setTimeout(function () {
+            i++;
+            if (i < 100) {
+                loading.innerHTML = "0" + i
+                myLoop();
+            } else {
+                loadingcomplete();
+                loading.innerHTML = "100";
+            }
+
+        }, 0)
+    }
 
     myLoop();
     gsap.set('.nav-list', { y: -70, opacity: 0 })
     gsap.set('.hero-title-col ', { x: 100, opacity: 0 })
     gsap.set('.hero-col-image', { x: -100, opacity: 0 })
     gsap.set('.burger', { x: '-100vw' })
-    function loadingcomplete() {
+
         gsap.to(".loading", { delay: 2.0, x: '-100' })
         var blist_item = document.querySelectorAll(".blist-item");
         var footer_link = document.querySelectorAll(".footer-link");
@@ -40,7 +41,8 @@ function myLoop() {
             burger.style.display = "none";
             var width = window.innerWidth;
             if (width > 700) {
-                hero_col_image.style.display = "none";
+                gsap.to(".hero-col-image", {x: "100vw" , duration: 1,   ease: "none", 
+                })
             }
             checkwidth();
             hero_title_col.classList.add("burger-active");
@@ -56,7 +58,9 @@ function myLoop() {
             checkwidth();
             var width = window.innerWidth;
             if (width > 700) {
-                hero_col_image.style.display = "block";
+
+                gsap.to(".hero-col-image", {x: 0 , duration: 1,   ease: "none", 
+                })
             }
             document.querySelector(".hero-title-col").style.left = document.querySelector(".first-item").offsetLeft + "px";
         }
@@ -85,10 +89,8 @@ function myLoop() {
         const gotoblock_reviews = document.querySelector(".reviews");
         const gotoblockvalue_reviews = gotoblock_reviews.getBoundingClientRect().top + scrollY - document.querySelector('.reviews').offsetHeight;
         const ginput_btn = document.querySelector(".input-btn");
-        const error_tel = document.querySelector(".error-tel"),
-        error_email = document.querySelector(".error-email"),
-            error_name = document.querySelector(".error-name");
-            var error = 0;
+
+        var error = 0;
         ginput_btn.addEventListener("click", function () {
             error_tel.style.display = "none";
             error_email.style.display = "none";
@@ -97,7 +99,7 @@ function myLoop() {
             ValidMail();
             ValidPhone();
             ValidName();
-            alert(error)
+
         })
         var myMail = document.querySelector('.input-email');
         var myName = document.querySelector('.input-name');
@@ -105,44 +107,46 @@ function myLoop() {
         var maskOptions = {
             mask: '+375 00-000-0000',
             lazy: false
-        } 
+        }
         var maskOptionsremove = {
             mask: null,
             lazy: false
-        } 
+        }
         IMask(myPhone, maskOptions);
-    
+
         function ValidMail() {
             var re = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
-            
+
             var valid = re.test(myMail.value);
             var output;
             if (valid) output = 'Адрес эл. почты введен правильно!';
             else {
                 error_email.style.display = "block";
                 error = 1;
-            } 
+            }
         }
         function ValidName() {
             var output;
-            if (myName.value!="") output = 'Адрес эл. почты введен правильно!';
+            if (myName.value !== "") output = 'Имя введено правильно!';
             else {
                 error_name.style.display = "block";
+                
                 error = 1;
-            } 
+            }
         }
         function ValidPhone() {
             var re = /^[\d\+][\d\(\)\ -]{4,14}\d$/;
-            
+
             var valid = re.test(myPhone.value);
             var output;
             if (valid) output = 'Номер телефона введен правильно!';
             else {
-                error_phone.style.display = "block";
+                error_tel.style.display = "block";
+
                 error = 1;
-            } 
-        }  
-    
+            }
+        }
+
         footer_link.forEach((el, index) => {
             el.addEventListener("click", (e) => {
                 burger_menu.classList.remove("--active");
@@ -184,6 +188,12 @@ function myLoop() {
                 }
             })
         })
+        var feedback_btn = document.querySelector(".feedback-btn");
+        feedback_btn.addEventListener("click", function () {
+            gotoblock_feedback.scrollIntoView({
+                duration: 2000
+            })
+        })
         blist_item.forEach((el, index) => {
             el.addEventListener("click", (e) => {
                 burger_menu.classList.remove("--active");
@@ -219,6 +229,10 @@ function myLoop() {
                         duration: 2000
                     })
                 } else if (index == 6) {
+                    gotoblock_feedback.scrollIntoView({
+                        duration: 2000
+                    })
+                } else if (index == 7) {
                     gotoblock_feedback.scrollIntoView({
                         duration: 2000
                     })
@@ -278,7 +292,7 @@ function myLoop() {
         //             alert(data);
         //         }
         //     });
-            
+
         // });
         const form = document.getElementById("order-form");
         form.addEventListener("submit", formSend);
@@ -290,227 +304,225 @@ function myLoop() {
                 body: formData
             })
         }
-    
-    
 
-        gsap.to('.burger', { x: '0', delay: 2.0,})
-        gsap.to(".nav-list", { delay: 2.0, y: 0, opacity: 1})
+
+
+        gsap.to('.burger', { x: '0', delay: 2.0, })
+        gsap.to(".nav-list", { delay: 2.0, y: 0, opacity: 1 })
         gsap.to('.hero-col-image', { x: 0, delay: 3, opacity: 1 })
-        gsap.to('.hero-title-col ', {x: 0, opacity: 1, delay: 2.0})
+        gsap.to('.hero-title-col ', { x: 0, opacity: 1, delay: 2.0 })
         gsap.to(".layer-1", { delay: .5, y: '-100vh' })
         gsap.to(".layer-2", { delay: .7, y: '-100vh' })
         gsap.to(".layer-3", { delay: .9, y: '-100vh' })
         gsap.to(".overlay", { delay: 1.5, y: '-100vh' })
         gsap.registerPlugin(ScrollTrigger);
-gsap.to(".title-company", { 
-    scrollTrigger: {
-        trigger: ".title-company",
-        start: "top +=700",
-        toggleActions: "play none none none"
-    },
-    x: 0,
-    opacity: 1
-})
-gsap.to(".animation-0", { 
-    scrollTrigger: {
-        trigger: ".animation-0",
-        start: "top +=700",
-        toggleActions: "play none none none"
-    },
-    x: 0,
-    opacity: 1
-})
-gsap.to(".animation-1", { 
-    scrollTrigger: {
-        trigger: ".animation-1",
-        start: "top +=700",
-        toggleActions: "play none none none"
-    },
-    x: 0,
-    opacity: 1
-})
-gsap.to(".advantages-title", { 
-    scrollTrigger: {
-        trigger: ".advantages-title",
-        start: "top +=700",
-        toggleActions: "play none none none"
-    },
-    x: 0,
-    opacity: 1
-})
-gsap.to(".animation-2", { 
-    scrollTrigger: {
-        trigger: ".animation-2",
-        start: "top +=700",
-        toggleActions: "play none none none"
-    },
-    y: 0,
-    opacity: 1
-})
-gsap.to(".animation-3", { 
-    scrollTrigger: {
-        trigger: ".animation-3",
-        start: "top +=700",
-        toggleActions: "play none none none"
-    },
-    y: 0,
-    opacity: 1
-})
-gsap.to(".price-title", { 
-    scrollTrigger: {
-        trigger: ".price-title ",
-        start: "top +=700",
-        toggleActions: "play none none none"
-    },
-    x: 0,
-    opacity: 1
-})
-gsap.to(".animation-4", { 
-    scrollTrigger: {
-        trigger: ".animation-4",
-        start: "top +=700",
-        toggleActions: "play none none none"
-    },
-    scale: 1,
-    opacity: 1
-})
-gsap.to(".animation-5", { 
-    scrollTrigger: {
-        trigger: ".animation-5",
-        start: "top +=700",
-        toggleActions: "play none none none"
-    },
-    scale: 1,
-    opacity: 1
-})
-gsap.to(".reviews-title", { 
-    scrollTrigger: {
-        trigger: ".reviews-title",
-        start: "top +=700",
-        toggleActions: "play none none none"
-    },
-    x: 0,
-    opacity: 1
-})
-gsap.to(".animation-6", { 
-    scrollTrigger: {
-        trigger: ".animation-6",
-        start: "top +=700",
-        toggleActions: "play none none none"
-    },
-    scaleY: 1,
-    opacity: 1
-})
-gsap.to(".animation-7", { 
-    scrollTrigger: {
-        trigger: ".animation-7",
-        start: "top +=700",
-        toggleActions: "play none none none"
-    },
-    scaleY: 1,
-    opacity: 1
-})
-gsap.to(".animation-8", { 
-    scrollTrigger: {
-        trigger: ".animation-8",
-        start: "top +=700",
-        toggleActions: "play none none none"
-    },
-    scaleX: 1,
-    opacity: 1
-})
-gsap.to(".animation-9", { 
-    scrollTrigger: {
-        trigger: ".animation-9",
-        start: "top +=700",
-        toggleActions: "play none none none"
-    },
-    scaleX: 1,
-    opacity: 1
-})
-gsap.to(".animation-10", { 
-    scrollTrigger: {
-        trigger: ".animation-10",
-        start: "top +=700",
-        toggleActions: "play none none none"
-    },
-    scaleX: 1,
-    opacity: 1
-})
+        gsap.to(".title-company", {
+            scrollTrigger: {
+                trigger: ".title-company",
+                start: "top +=700",
+                toggleActions: "play none none none"
+            },
+            x: 0,
+            opacity: 1
+        })
+        gsap.to(".animation-0", {
+            scrollTrigger: {
+                trigger: ".animation-0",
+                start: "top +=700",
+                toggleActions: "play none none none"
+            },
+            x: 0,
+            opacity: 1
+        })
+        gsap.to(".animation-1", {
+            scrollTrigger: {
+                trigger: ".animation-1",
+                start: "top +=700",
+                toggleActions: "play none none none"
+            },
+            x: 0,
+            opacity: 1
+        })
+        gsap.to(".advantages-title", {
+            scrollTrigger: {
+                trigger: ".advantages-title",
+                start: "top +=700",
+                toggleActions: "play none none none"
+            },
+            x: 0,
+            opacity: 1
+        })
+        gsap.to(".animation-2", {
+            scrollTrigger: {
+                trigger: ".animation-2",
+                start: "top +=700",
+                toggleActions: "play none none none"
+            },
+            y: 0,
+            opacity: 1
+        })
+        gsap.to(".animation-3", {
+            scrollTrigger: {
+                trigger: ".animation-3",
+                start: "top +=700",
+                toggleActions: "play none none none"
+            },
+            y: 0,
+            opacity: 1
+        })
+        gsap.to(".price-title", {
+            scrollTrigger: {
+                trigger: ".price-title ",
+                start: "top +=700",
+                toggleActions: "play none none none"
+            },
+            x: 0,
+            opacity: 1
+        })
+        gsap.to(".animation-4", {
+            scrollTrigger: {
+                trigger: ".animation-4",
+                start: "top +=700",
+                toggleActions: "play none none none"
+            },
+            scale: 1,
+            opacity: 1
+        })
+        gsap.to(".animation-5", {
+            scrollTrigger: {
+                trigger: ".animation-5",
+                start: "top +=700",
+                toggleActions: "play none none none"
+            },
+            scale: 1,
+            opacity: 1
+        })
+        gsap.to(".reviews-title", {
+            scrollTrigger: {
+                trigger: ".reviews-title",
+                start: "top +=700",
+                toggleActions: "play none none none"
+            },
+            x: 0,
+            opacity: 1
+        })
+        gsap.to(".animation-6", {
+            scrollTrigger: {
+                trigger: ".animation-6",
+                start: "top +=700",
+                toggleActions: "play none none none"
+            },
+            scaleY: 1,
+            opacity: 1
+        })
+        gsap.to(".animation-7", {
+            scrollTrigger: {
+                trigger: ".animation-7",
+                start: "top +=700",
+                toggleActions: "play none none none"
+            },
+            scaleY: 1,
+            opacity: 1
+        })
+        gsap.to(".animation-8", {
+            scrollTrigger: {
+                trigger: ".animation-8",
+                start: "top +=700",
+                toggleActions: "play none none none"
+            },
+            scaleX: 1,
+            opacity: 1
+        })
+        gsap.to(".animation-9", {
+            scrollTrigger: {
+                trigger: ".animation-9",
+                start: "top +=700",
+                toggleActions: "play none none none"
+            },
+            scaleX: 1,
+            opacity: 1
+        })
+        gsap.to(".animation-10", {
+            scrollTrigger: {
+                trigger: ".animation-10",
+                start: "top +=700",
+                toggleActions: "play none none none"
+            },
+            scaleX: 1,
+            opacity: 1
+        })
 
-gsap.to(".animation-11", { 
-    scrollTrigger: {
-        trigger: ".animation-11",
-        start: "top +=700",
-        toggleActions: "play none none none"
-    },
-    scaleX: 1,
-    opacity: 1
-})
-gsap.to(".animation-12", { 
-    scrollTrigger: {
-        trigger: ".animation-12",
-        start: "top +=700",
-        toggleActions: "play none none none"
-    },
-    scaleX: 1,
-    opacity: 1
-})
-gsap.to(".animation-13", { 
-    scrollTrigger: {
-        trigger: ".animation-13",
-        start: "top +=700",
-        toggleActions: "play none none none"
-    },
-    scaleX: 1,
-    opacity: 1
-})
-gsap.to(".animation-14", { 
-    scrollTrigger: {
-        trigger: ".animation-14",
-        start: "top +=700",
-        toggleActions: "play none none none"
-    },
-    scaleX: 1,
-    opacity: 1
-})
-gsap.to(".animation-15", { 
-    scrollTrigger: {
-        trigger: ".animation-15",
-        start: "top +=700",
-        toggleActions: "play none none none"
-    },
-    scaleX: 1,
-    opacity: 1
-})
-gsap.to(".animation-16", { 
-    scrollTrigger: {
-        trigger: ".animation-16",
-        start: "top +=700",
-        toggleActions: "play none none none"
-    },
-    scaleX: 1,
-    opacity: 1
-})
-gsap.to(".animation-17", { 
-    scrollTrigger: {
-        trigger: ".animation-17",
-        start: "top +=700",
-        toggleActions: "play none none none"
-    },
-    scaleX: 1,
-    opacity: 1
-})
-gsap.to(".animation-18", { 
-    scrollTrigger: {
-        trigger: ".animation-18",
-        start: "top +=700",
-        toggleActions: "play none none none"
-    },
-    opacity: 1
-})
-
-
+        gsap.to(".animation-11", {
+            scrollTrigger: {
+                trigger: ".animation-11",
+                start: "top +=700",
+                toggleActions: "play none none none"
+            },
+            scaleX: 1,
+            opacity: 1
+        })
+        gsap.to(".animation-12", {
+            scrollTrigger: {
+                trigger: ".animation-12",
+                start: "top +=700",
+                toggleActions: "play none none none"
+            },
+            scaleX: 1,
+            opacity: 1
+        })
+        gsap.to(".animation-13", {
+            scrollTrigger: {
+                trigger: ".animation-13",
+                start: "top +=700",
+                toggleActions: "play none none none"
+            },
+            scaleX: 1,
+            opacity: 1
+        })
+        gsap.to(".animation-14", {
+            scrollTrigger: {
+                trigger: ".animation-14",
+                start: "top +=700",
+                toggleActions: "play none none none"
+            },
+            scaleX: 1,
+            opacity: 1
+        })
+        gsap.to(".animation-15", {
+            scrollTrigger: {
+                trigger: ".animation-15",
+                start: "top +=700",
+                toggleActions: "play none none none"
+            },
+            scaleX: 1,
+            opacity: 1
+        })
+        gsap.to(".animation-16", {
+            scrollTrigger: {
+                trigger: ".animation-16",
+                start: "top +=700",
+                toggleActions: "play none none none"
+            },
+            scaleX: 1,
+            opacity: 1
+        })
+        gsap.to(".animation-17", {
+            scrollTrigger: {
+                trigger: ".animation-17",
+                start: "top +=700",
+                toggleActions: "play none none none"
+            },
+            scaleX: 1,
+            opacity: 1
+        })
+        gsap.to(".animation-18", {
+            scrollTrigger: {
+                trigger: ".animation-18",
+                start: "top +=700",
+                toggleActions: "play none none none"
+            },
+            opacity: 1
+        })
 
 
 
@@ -523,8 +535,10 @@ gsap.to(".animation-18", {
 
 
 
-    }
+
+
     
+
 
 })
 
